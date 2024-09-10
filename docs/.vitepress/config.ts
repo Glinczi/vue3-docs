fd4b1c98de520dad31e3f16f17411e462b77f648
gongcunzhi <gcz9956@outlook.com>
2024-09-10 14:55:20 +0800

    ✨ feat: 配置修改

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "Vue Docs",
   description: "Vue3",
-  base:'/vu3-docs/',
+  base:'/vue3-docs/',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
